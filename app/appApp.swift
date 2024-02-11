1f19b54448d259234e0a921abf31abb9fee2cc2b
seungdeok <akk9132@naver.com>
2024-02-12 01:12:16 +0900

    feat: Configure firebase sdk

--- app/appApp.swift
+++ app/appApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct appApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
