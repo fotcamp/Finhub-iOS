c8dfd1a72eca9983c5131c0688c3fb2cd01f1c3d
seungdeok <akk9132@naver.com>
2024-02-12 21:38:06 +0900

    fix: 키패드 노출 시 높이 깨짐 현상 수정 요청

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -6,10 +6,22 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @State private var keyboardHeight: CGFloat = 0
+
     var body: some View {
-        Webview(url: URL(string: "http://finhub-front-end.vercel.app/")!).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        Webview(url: URL(string: "http://finhub-front-end.vercel.app/")!)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { notification in
+                if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                    self.keyboardHeight = keyboardFrame.height
+                }
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
+                self.keyboardHeight = 0
+            }
     }
 }
 
