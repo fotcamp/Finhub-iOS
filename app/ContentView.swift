c8da1491652be1b78e96a702dad531357351d8ff
seungdeok <akk9132@naver.com>
2024-02-12 23:10:19 +0900

    chore: Set safearea color

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -12,16 +12,21 @@ struct ContentView: View {
     @State private var keyboardHeight: CGFloat = 0
 
     var body: some View {
-        Webview(url: URL(string: "http://finhub-front-end.vercel.app/")!)
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { notification in
-                if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-                    self.keyboardHeight = keyboardFrame.height
+        ZStack(content: {
+            Color.white
+                .edgesIgnoringSafeArea(.top)
+            Color.white
+                .edgesIgnoringSafeArea(.bottom)
+            Webview(url: URL(string: "http://finhub-front-end.vercel.app/")!)
+                .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { notification in
+                    if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                        self.keyboardHeight = keyboardFrame.height
+                    }
                 }
-            }
-            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
-                self.keyboardHeight = 0
-            }
+                .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
+                    self.keyboardHeight = 0
+                }
+        })
     }
 }
 
